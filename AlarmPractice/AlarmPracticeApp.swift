288dfc43674b13d208d3eb6a927274a9f61ba608
Greed <suby0475@gmail.com>
2023-05-08 21:00:27 +0900

    Initial Commit

---
+++ AlarmPractice/AlarmPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmPracticeApp.swift
+//  AlarmPractice
+//
+//  Created by Greed on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct AlarmPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
